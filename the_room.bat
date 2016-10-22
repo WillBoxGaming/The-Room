9fd6dd24127aa6bfb0eff13d02119cca3df4ecb6
WillBoxGaming <willboxgaming1@gmail.com>
2016-10-22 01:27:08 -0400

    Added startup

---
+++ the_room.bat
@@ -0,0 +1,285 @@
+@echo off
+cls
+title The Room
+mode con:cols=100 lines=40
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                                           Loading...
+set night=night1
+set name=You
+(
+echo night=%night%
+echo name=%name%
+) > save.sav
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                For a better experience, please close the lights and put the sound on.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+PING 1.1.1.1 -n 1 -w 6000 >NUL
+cls
+color 0C
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                For a better experience, please close the lights and put the sound on.
+echo.
+echo.
+echo.
+echo.
+echo                                   STRANGER: DO IT OR I'LL KILL YOU.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+PING 1.1.1.1 -n 1 -w 500 >NUL
+cls
+color 0F
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                         WillBoxGaming presents
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+PING 1.1.1.1 -n 1 -w 6000 >NUL
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                         A game made in MS-DOS
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+PING 1.1.1.1 -n 1 -w 6000 >NUL
+cls
+PING 1.1.1.1 -n 1 -w 6000 >NUL
+:menu
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                              THE ROOM
+echo.
+echo                                            (Don't) Play
+echo.
+echo                                             How to play
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+set /p choice="You: "
+if choice == play goto play
+if choice == "(Dont't) play" goto play
+if choice == "Dont't play" goto play
+if choice == "Dont play" goto play
+if choice == how goto how
+if choice == "How to play" goto how
+if choice == "How 2 play" goto how
+goto kiddingmenace
+:kiddingmenace
+cls
+PING 1.1.1.1 -n 1 -w 2000 >NUL
+color 0C
+echo Stranger: Are you kidding me? Write something written in the menu! DO YOU WAN'T YOUR FAMILLY TO GET KILLED?!
+set /p random82374238947="You: "
+PING 1.1.1.1 -n 1 -w 1200 >NUL
+color 0F
+echo stranger: Yeah, whatever.
+PING 1.1.1.1 -n 1 -w 2000 >NUL
+goto menu
+:how
+echo Stranger: bleh.
+set /p random67234846327462379="You: "
+echo Stranger: Bleh bleh bleh.
+PING 1.1.1.1 -n 1 -w 1200 >NUL
+goto menu
